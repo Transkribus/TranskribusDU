e874edf7f687cc3f42dc05d1afb117dcc541ad1e
Jean-Luc Meunier <jean-luc.meunier@xrce.xerox.com>
2017-04-21 15:16:18 +0200

    merge with code from RV's table branch - wiki-documented test passed

---
+++ src/crf/FeatureDefinition_PageXml_std_noText.py
@@ -0,0 +1,111 @@
+# -*- coding: utf-8 -*-
+
+"""
+    Standard PageXml features
+
+    Copyright Xerox(C) 2016 JL. Meunier
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    
+    
+    Developed  for the EU project READ. The READ project has received funding 
+    from the European Union�s Horizon 2020 research and innovation programme 
+    under grant agreement No 674943.
+    
+"""
+
+import numpy as np
+
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.preprocessing import StandardScaler
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+from crf.Transformer import SparseToDense
+from crf.Transformer_PageXml import NodeTransformerTextEnclosed, NodeTransformerTextLen, NodeTransformerXYWH_v2, NodeTransformerNeighbors, Node1HotFeatures
+from crf.Transformer_PageXml import Edge1HotFeatures, EdgeBooleanFeatures, EdgeNumericalSelector, EdgeTransformerSourceText, EdgeTransformerTargetText
+from crf.PageNumberSimpleSequenciality import PageNumberSimpleSequenciality
+
+from FeatureDefinition import FeatureDefinition
+
+class FeatureDefinition_PageXml_StandardOnes_noText(FeatureDefinition):
+    
+    def __init__(self): 
+        FeatureDefinition.__init__(self)
+        
+#         self.n_tfidf_node, self.t_ngrams_node, self.b_tfidf_node_lc = n_tfidf_node, t_ngrams_node, b_tfidf_node_lc
+#         self.n_tfidf_edge, self.t_ngrams_edge, self.b_tfidf_edge_lc = n_tfidf_edge, t_ngrams_edge, b_tfidf_edge_lc
+
+#         tdifNodeTextVectorizer = TfidfVectorizer(lowercase=self.b_tfidf_node_lc, max_features=self.n_tfidf_node
+#                                                                                   , analyzer = 'char', ngram_range=self.t_ngrams_node #(2,6)
+#                                                                                   , dtype=np.float64)
+        
+        node_transformer = FeatureUnion( [  #CAREFUL IF YOU CHANGE THIS - see cleanTransformers method!!!!
+                                    ("xywh", Pipeline([
+                                                         ('selector', NodeTransformerXYWH_v2()),
+                                                         ('xywh', StandardScaler(copy=False, with_mean=True, with_std=True))  #use in-place scaling
+                                                         ])
+                                       )
+                                    , ("neighbors", Pipeline([
+                                                         ('selector', NodeTransformerNeighbors()),
+                                                         ('neighbors', StandardScaler(copy=False, with_mean=True, with_std=True))  #use in-place scaling
+                                                         ])
+                                       )
+                                    , ("1hot", Pipeline([
+                                                         ('1hot', Node1HotFeatures())  #does the 1-hot encoding directly
+                                                         ])
+                                       )
+                                      ])
+    
+        lEdgeFeature = [  #CAREFUL IF YOU CHANGE THIS - see cleanTransformers method!!!!
+                                      ("1hot", Pipeline([
+                                                         ('1hot', Edge1HotFeatures(PageNumberSimpleSequenciality()))
+                                                         ])
+                                        )
+                                    , ("boolean", Pipeline([
+                                                         ('boolean', EdgeBooleanFeatures())
+                                                         ])
+                                        )
+                                    , ("numerical", Pipeline([
+                                                         ('selector', EdgeNumericalSelector()),
+                                                         ('numerical', StandardScaler(copy=False, with_mean=True, with_std=True))  #use in-place scaling
+                                                         ])
+                                        )
+                        ]
+                        
+        edge_transformer = FeatureUnion( lEdgeFeature )
+          
+        #return _node_transformer, _edge_transformer, tdifNodeTextVectorizer
+        self._node_transformer = node_transformer
+        self._edge_transformer = edge_transformer
+        self.tfidfNodeTextVectorizer = None #tdifNodeTextVectorizer
+        
+    def getTransformers(self):
+        """
+        return (node transformer, edge transformer)
+        """
+        return self._node_transformer, self._edge_transformer
+    
+#     def cleanTransformers(self):
+#         """
+#         the TFIDF transformers are keeping the stop words => huge pickled file!!!
+#         
+#         Here the fix is a bit rough. There are better ways....
+#         JL
+#         """
+#         self._node_transformer.transformer_list[0][1].steps[1][1].stop_words_ = None   #is 1st in the union...
+#         for i in [2, 3, 4, 5, 6, 7]:
+#             self._edge_transformer.transformer_list[i][1].steps[1][1].stop_words_ = None   #are 3rd and 4th in the union....
+#         return self._node_transformer, self._edge_transformer        
+
+    
---
+++ src/crf/Graph_Multi_SinglePageXml.py
@@ -0,0 +1,181 @@
+# -*- coding: utf-8 -*-
+
+"""
+    Computing the graph for a MultiPageXml document
+
+    Copyright Xerox(C) 2017 H . Déjean
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    
+    
+    Developed  for the EU project READ. The READ project has received funding 
+    from the European Union�s Horizon 2020 research and innovation programme 
+    under grant agreement No 674943.
+    
+"""
+import libxml2
+
+from Graph_MultiPageXml import Graph_MultiPageXml
+from common.trace import traceln
+from xml_formats.PageXml import PageXml
+import Edge
+from Page import Page
+
+class Graph_MultiSinglePageXml(Graph_MultiPageXml):
+    '''
+    Computing the graph for a MultiPageXml document
+
+        USAGE:
+        - call parseFile to load the DOM and create the nodes and edges
+        - call detachFromDOM before freeing the DOM
+    '''
+    #Namespace, of PageXml, at least
+    dNS = {"pc":PageXml.NS_PAGE_XML}
+    
+    #How to list the pages of a (Multi)PageXml doc
+    sxpPage     = "//pc:Page"
+
+    def __init__(self, lNode = [], lEdge = []):
+        Graph_MultiPageXml.__init__(self, lNode, lEdge)
+
+
+
+    @classmethod
+    def loadGraphs(cls, lsFilename, bNeighbourhood=True, bDetach=False, bLabelled=False, iVerbose=0):
+        """
+        Load one graph per file, and detach its DOM
+        return the list of loaded graphs
+        """
+        lGraph = []
+        for sFilename in lsFilename:
+            if iVerbose: traceln("\t%s"%sFilename)
+            lG= Graph_MultiSinglePageXml.getSinglePages(sFilename, bNeighbourhood,bDetach,bLabelled, iVerbose)
+#             if bNeighbourhood: g.collectNeighbors()            
+#             if bLabelled: g.parseDomLabels()
+#             if bDetach: g.detachFromDOM()
+            lGraph.extend(lG)
+        return lGraph
+    
+    @classmethod
+    def getSinglePages(cls, sFilename,bNeighbourhood=True, bDetach=False, bLabelled=False, iVerbose=0):
+        """
+        load a pageXml
+        Return a CRF Graph object
+        """
+    
+        lGraph=[]
+        doc = libxml2.parseFile(sFilename)
+        #load the block of each page, keeping the list of blocks of previous page
+        lPrevPageNode = None
+
+        for pnum, page, domNdPage in cls._iter_Page_DomNode(doc):
+            g = cls()
+            g.doc= doc
+            
+            g.lNode,  g.lEdge = list(), list()
+            #now that we have the page, let's create the node for each type!
+            lPageNode = list()
+            setPageNdDomId = set() #the set of DOM id
+            # because the node types are supposed to have an empty intersection
+                            
+            lPageNode = [nd for nodeType in g.getNodeTypeList() for nd in nodeType._iter_GraphNode(doc, domNdPage, page) ]
+
+            #check that each node appears once
+            setPageNdDomId = set([nd.domid for nd in lPageNode])
+            assert len(setPageNdDomId) == len(lPageNode), "ERROR: some nodes fit with multiple NodeTypes"
+            
+        
+            g.lNode = lPageNode
+            lPageEdge = Edge.Edge.computeEdges(lPrevPageNode, lPageNode)
+                
+            g.lEdge = lPageEdge
+            if iVerbose>=2: traceln("\tPage %5d    %6d nodes    %7d edges"%(pnum, len(lPageNode), len(lPageEdge)))
+            
+            if not g.isEmpty() and len(g.lEdge) > 0:    
+                if bNeighbourhood: g.collectNeighbors()            
+                if bLabelled: g.parseDomLabels()
+    #             if bDetach: g.detachFromDOM()
+    
+                lGraph.append(g)
+            if iVerbose: traceln("\t\t (%d nodes,  %d edges)"%(len(g.lNode), len(g.lEdge)) )
+        
+        return lGraph     
+       
+#     def parseXmlFile(self, sFilename, iVerbose=0):
+#         """
+#         load a pageXml
+#         Return a CRF Graph object
+#         """
+#     
+#         lGraph=[]
+#         self.doc = libxml2.parseFile(sFilename)
+#         self.lNode, self.lEdge = list(), list()
+#         #load the block of each page, keeping the list of blocks of previous page
+#         lPrevPageNode = None
+# 
+#         for pnum, page, domNdPage in self._iter_Page_DomNode(self.doc):
+#             #now that we have the page, let's create the node for each type!
+#             lPageNode = list()
+#             setPageNdDomId = set() #the set of DOM id
+#             # because the node types are supposed to have an empty intersection
+#                             
+#             lPageNode = [nd for nodeType in self.getNodeTypeList() for nd in nodeType._iter_GraphNode(self.doc, domNdPage, page) ]
+#             
+#             #check that each node appears once
+#             setPageNdDomId = set([nd.domid for nd in lPageNode])
+#             assert len(setPageNdDomId) == len(lPageNode), "ERROR: some nodes fit with multiple NodeTypes"
+#             
+#         
+#             self.lNode.extend(lPageNode)
+#             
+#             lPageEdge = Edge.Edge.computeEdges(lPrevPageNode, lPageNode)
+#             
+#             self.lEdge.extend(lPageEdge)
+#             if iVerbose>=2: traceln("\tPage %5d    %6d nodes    %7d edges"%(pnum, len(lPageNode), len(lPageEdge)))
+#             
+#             lPrevPageNode = lPageNode
+#         if iVerbose: traceln("\t\t (%d nodes,  %d edges)"%(len(self.lNode), len(self.lEdge)) )
+#         
+#         return lGraph    
+
+    # ---------------------------------------------------------------------------------------------------------
+    @classmethod
+    def _iter_Page_DomNode(cls, doc):
+        """
+        Parse a Multi-pageXml DOM, by page
+
+        iterator on the DOM, that returns per page:
+            page-num (int), page object, page dom node
+        
+        """
+        #--- XPATH contexts
+        ctxt = doc.xpathNewContext()
+        for ns, nsurl in Graph_MultiSinglePageXml.dNS.items(): ctxt.xpathRegisterNs(ns, nsurl)
+        
+        assert Graph_MultiSinglePageXml.sxpPage, "CONFIG ERROR: need an xpath expression to enumerate PAGE elements"
+        lNdPage = ctxt.xpathEval(Graph_MultiSinglePageXml.sxpPage)   #all pages
+        pnum = 0
+        pagecnt = len(lNdPage)
+        for ndPage in lNdPage:
+            pnum += 1
+            iPageWidth  = int( ndPage.prop("imageWidth") )
+            iPageHeight = int( ndPage.prop("imageHeight") )
+            page = Page(pnum, pagecnt, iPageWidth, iPageHeight, cls=None, domnode=ndPage, domid=ndPage.prop("id"))
+            yield (pnum, page, ndPage)
+            
+        ctxt.xpathFreeContext()       
+        
+        raise StopIteration()        
+               
+        
