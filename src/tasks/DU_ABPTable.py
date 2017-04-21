09054677b1c7a2fe2f2688f014f1ecc56b8d0b34
Jean-Luc Meunier <jean-luc.meunier@xrce.xerox.com>
2017-04-21 15:26:12 +0200

    back to RV's settings

--- src/tasks/DU_ABPTable.py
+++ src/tasks/DU_ABPTable.py
@@ -113,11 +113,11 @@ class DU_ABPTable(DU_CRF_Task):
                      , dFeatureConfig = {  }
                      , dLearnerConfig = {
                             'C'                : .1 
-                         , 'njobs'            : 3
+                         , 'njobs'            : 8
                          , 'inference_cache'  : 10
                         , 'tol'              : .1
                         , 'save_every'       : 50     #save every 50 iterations,for warm start
-                         , 'max_iter'         : 2
+                         , 'max_iter'         : 1000
                          }
                      , sComment=sComment
                      ,cFeatureDefinition=FeatureDefinition_PageXml_StandardOnes_noText
