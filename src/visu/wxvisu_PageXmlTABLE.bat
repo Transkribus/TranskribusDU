fd1ce995ab3079982047b5f66bb29029169c19c6
Jean-Luc Meunier <jean-luc.meunier@xrce.xerox.com>
2017-04-24 11:33:51 +0200

    .ini for ABP tables

---
+++ src/visu/wxvisu_PageXmlTABLE.bat
@@ -0,0 +1,2 @@
+rem c:\python27\python.exe c:\local\meunier\git\DLA\src\visu\wxvisu\wxvisu.py c:\local\meunier\git\DLA\src\visu\wxvisu\wxvisu.ini %1
+C:\Anaconda2\python.exe c:\local\meunier\git\DLA\src\visu\wxvisu\wxvisu.py %0.ini %1
