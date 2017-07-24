755242a3e0f3a361cb43098fcb245d61f5788b33
Stephane Clinchant <sclincha@michigan.grenoble.xrce.xerox.com>
2017-07-24 12:02:44 +0200

    Edge GCN Model

--- src/gcn/tests/UT_gcn.py
+++ src/gcn/tests/UT_gcn.py
@@ -2,7 +2,7 @@
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
-
+import pdb
 __author__ = 'sclincha'
 
 
@@ -14,7 +14,7 @@ import numpy as np
 import unittest
 import pickle
 import tensorflow as tf
-
+import scipy.sparse as sp
 
 try: #to ease the use without proper Python installation
     import TranskribusDU_version
@@ -32,6 +32,7 @@ from gcn.gcn_datasets import GCNDataset
 from gcn.gcn_models import GCNModel
 
 def make_fake_gcn_dataset():
+    #TODO Fix this this is not compatible with version 3.0
     X,Y=load_iris(return_X_y=True)
 
 
@@ -137,6 +138,96 @@ class UT_gcn(unittest.TestCase):
         lr.fit(dataset.X,np.argmax(dataset.Y,axis=1))
         acc=lr.score(dataset.X,np.argmax(dataset.Y,axis=1))
         print('Accuracy LR',acc)
+
+    def test_03_buildEdgeMat(self):
+        dataset=GCNDataset('UT_iris_0')
+        dataset.load_pickle('iris_graph.pickle')
+        dataset.print_stats()
+
+        A=dataset.A
+        E=dataset.E
+        print(A.shape)
+        print(E.shape)
+        nb_node = A.shape[0]
+        edge_dim= dataset.E.shape[1]-2 #Preprocess That
+
+        EA =np.zeros((edge_dim,(nb_node*nb_node)),dtype=np.float32)
+        #edge_idx=list(zip(E[:,0],E[:,1]))
+        #edge_idx = [(int(x[0]),int(x[1])) for x in edge_idx]
+        i_list =[]
+        j_list=[]
+        for x,y in zip(E[:,0],E[:,1]):
+            i_list.append(int(x))
+            j_list.append(int(y))
+
+        for i in range(edge_dim):
+            #Build a adjecency sparse matrix for the i_dim of the edge
+            #pdb.set_trace()
+            idim_mat =sp.coo_matrix((E[:,i+2],(i_list,j_list)), shape=(nb_node, nb_node))
+
+            D= np.asarray(idim_mat.todense()).squeeze()
+            EA[i,:]=np.reshape(D,-1)
+
+        print(EA.shape)
+        print(E[0:4,:])
+
+        #Print rep of first edge
+        print('Representation of first node')
+        print(EA[:,int(E[0,1])])
+        print(EA[:,int(E[1,1])])
+        #Print rep of first edge
+        print('Representation of second node')
+        print(EA[:,nb_node+int(E[2,1])])
+        print(EA[:,nb_node+int(E[3,1])])
+
+
+        Wedge  = tf.Variable(tf.ones([1,edge_dim], dtype=np.float32, name='Wedge'))
+
+        tf_EA=tf.constant(EA)
+
+        Em =(tf.matmul(Wedge,tf_EA))
+        Z=tf.reshape(Em,(nb_node,nb_node))
+
+        init = tf.global_variables_initializer()
+        with tf.Session() as session:
+            session.run(init)
+            Em_=session.run(Em)
+            Z_=session.run(Z)
+            print(Em_.shape)
+            print(Z_.shape)
+            #TODO check somestuff on Z
+
+        #Build it once
+    def test_04_learn_edge(self):
+        dataset=GCNDataset('UT_iris_0')
+        dataset.load_pickle('iris_graph.pickle')
+        dataset.print_stats()
+
+        gcn_model = GCNModel(dataset,num_layers=1,learning_rate=0.1)
+        #gcn_model.activation=tf.nn.softmax
+        gcn_model.activation=tf.nn.relu
+        #gcn_model.activation=tf.nn.sigmoid
+        gcn_model.create_model()
+
+
+        edge_model =GCNModel(dataset,num_layers=1,learning_rate=0.1)
+        #gcn_model.activation=tf.nn.softmax
+        edge_model.activation=tf.nn.relu
+        edge_model.learn_edge=True
+        edge_model.create_model()
+
+
+        nb_iter=300
+        with tf.Session() as session:
+            session.run([gcn_model.init,edge_model.init])
+            gcn_model.train(session,n_iter=nb_iter)
+            print('Edge model')
+            edge_model.train(session,n_iter=nb_iter)
+            we =session.run(edge_model.Wedge)
+            print(we)
+        #Bug was due to two activation function of the logit ....
+
+
 
 
 
