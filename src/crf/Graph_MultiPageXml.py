b561ae7ff702c3dbf0bd58652a5b248149b252fb
Jean-Luc Meunier <jean-luc.meunier@naverlabs.com>
2017-08-11 15:34:00 +0200

    ContinuousPage mode for standard and Factorial graph

--- src/crf/Graph_MultiPageXml.py
+++ src/crf/Graph_MultiPageXml.py
@@ -31,9 +31,10 @@ import libxml2
 from common.trace import traceln
 
 from Graph import Graph
-import Edge
+from Transformer_PageXml import  EdgeTransformerClassShifter
+from Block import Block, BlockShallowCopy
+from Edge import Edge, HorizontalEdge, VerticalEdge, CrossPageEdge, CrossMirrorContinuousPageVerticalEdge
 from Page import Page
-
 from xml_formats.PageXml import PageXml
 
 class Graph_MultiPageXml(Graph):
@@ -81,7 +82,7 @@ class Graph_MultiPageXml(Graph):
         ctxt.xpathFreeContext()       
         
         raise StopIteration()        
-        
+
 # ------------------------------------------------------------------------------------------------------------------------------------------------
 class FactorialGraph_MultiPageXml(Graph_MultiPageXml):
     """
@@ -112,14 +113,6 @@ class FactorialGraph_MultiPageXml(Graph_MultiPageXml):
             if nt.__class__      != nt0.__class__     : raise ValueError("FactorialCRF requires all NodeType to be of same class.")
             if nt.getXpathExpr() != nt0.getXpathExpr(): raise ValueError("FactorialCRF requires all NodeType to have same Xpath selection expressions.")
 
-    # --- Faking single node type ----------------------------------------------------------
-    def showOneNodeType(self):
-        self._actual_lNodeType    = self._lNodeType
-        self.__class__._lNodeType = [self._actual_lNodeType[0]]
-
-    def restoreNodeTypeList(self):
-        self.__class__._lNodeType = self._actual_lNodeType
-            
     # --- Labels ----------------------------------------------------------
     def parseDomLabels(self):
         """
@@ -168,14 +161,38 @@ class FactorialGraph_MultiPageXml(Graph_MultiPageXml):
         We parse the XML for one of the node types
         That enough to get the list of nodes and edges. We can then have a virtually layered graph, one layer per node type
         """
-        self.showOneNodeType()
-        
-        ret = Graph_MultiPageXml.parseXmlFile(self, sFilename, iVerbose)
-        
-        self.restoreNodeTypeList()
+        self.doc = libxml2.parseFile(sFilename)
+        self.lNode, self.lEdge = list(), list()
+        #load the block of each page, keeping the list of blocks of previous page
+        lPrevPageNode = None
 
-        return ret
-
+        nodeType0 = self.getNodeTypeList()[0]    #all nodes have same type
+        
+        for pnum, page, domNdPage in self._iter_Page_DomNode(self.doc):
+            #now that we have the page, let's create the node for each type!
+            lPageNode = list()
+            setPageNdDomId = set() #the set of DOM id
+            # because the node types are supposed to have an empty intersection
+                            
+            lPageNode = list(nodeType0._iter_GraphNode(self.doc, domNdPage, page))
+            
+            #check that each node appears once
+            setPageNdDomId = set([nd.domid for nd in lPageNode])
+            assert len(setPageNdDomId) == len(lPageNode), "ERROR: some nodes fit with multiple NodeTypes"
+            
+        
+            self.lNode.extend(lPageNode)
+            
+            lPageEdge = Edge.computeEdges(lPrevPageNode, lPageNode)
+            
+            self.lEdge.extend(lPageEdge)
+            if iVerbose>=2: traceln("\tPage %5d    %6d nodes    %7d edges"%(pnum, len(lPageNode), len(lPageEdge)))
+            
+            lPrevPageNode = lPageNode
+        if iVerbose: traceln("\t\t (%d nodes,  %d edges)"%(len(self.lNode), len(self.lEdge)) )
+        
+        return self
+        
 
     def getY(self):
         """
@@ -254,4 +271,169 @@ class FactorialGraph_MultiPageXml(Graph_MultiPageXml):
         empty pages are skipped (and _not_ reflected as empty list)
         """
         raise Exception("Not implemented: getNodeIndexByPage. COnstraints not (yet) supported in factorialCRF.")
+        
+# ------------------------------------------------------------------------------------------------------------------------------------------------
+        
+class ContinousPage:
+    def computeContinuousPageEdges(self, lPrevPageEdgeBlk, lPageBlk, bMirror=True):
+        """
+        Consider pages in contunous mode, next page possibly left<-->right mirrored.
+        Create edge between horrizontally overlapping block of 2nd half of previous page and block of 1st half of next page
+        
+        Computation trick:
+            Create a fake page with lower half of previous page and upper half of current page.
+            Compute vertical links.
+            Only keep link from one page to the other. 
+        """
+        
+        lAllEdge = list()
+        
+        if lPrevPageEdgeBlk and lPageBlk:
+            #empty pages lead to empty return!
+            p0, p1 = lPrevPageEdgeBlk[0].getPage(), lPageBlk[0].getPage()
+            w0, h0 = p0.getWidthHeight()
+            w1, h1 = p1.getWidthHeight()
+            
+            p0_vertical_middle, p1_vertical_middle = h0/2.0 , h1/2.0
+            
+            #Shallow copy of half of pages (resp. lower and upper halfs)
+            lVirtualPageBlk  = [ BlockShallowCopy(blk) for blk in lPrevPageEdgeBlk if blk.getCenter()[1] >= p0_vertical_middle]
+            lNextHalfPage    = [ BlockShallowCopy(blk) for blk in lPageBlk        if blk.getCenter()[1] <= p1_vertical_middle]
+        
+            if lVirtualPageBlk and lNextHalfPage:
+                #if one half is empty, return no edge!
+                
+                #translate blocks to fit in a fake page
+                for blk in lVirtualPageBlk: blk.translate(0, -p0_vertical_middle)
+                for blk in lNextHalfPage  : blk.translate(0, +p0_vertical_middle) 
+                
+                #optionnaly mirroring them horizontally
+                if bMirror:
+                    for blk in lNextHalfPage: blk.mirrorHorizontally(w1)            
+                
+                lVirtualPageBlk.extend(lNextHalfPage)
+                lEdge = Block._findVerticalNeighborEdges(lVirtualPageBlk, CrossMirrorContinuousPageVerticalEdge)
+                
+                #keep only those edge accross pages, and make them to link original blocks!
+                lAllEdge = [CrossMirrorContinuousPageVerticalEdge(edge.A.getOrigBlock(), edge.B.getOrigBlock(), edge.length) \
+                            for edge in lEdge if edge.A.pnum != edge.B.pnum]
+            
+                if False and lAllEdge:
+                    print "---Page ", lAllEdge[0].A.pnum
+                    for edge in lAllEdge: print edge.A.node.prop("id"), " -->  ", edge.B.node.prop("id")
+                   
+        return lAllEdge
+
+# --------------------------------------------------------------------------------------
+    
+class Graph_MultiContinousPageXml(Graph_MultiPageXml, ContinousPage):
+    '''
+    Here we have edge between blocks of consecutives page as if they were in continuous arrangement
+    '''
+
+    def __init__(self, lNode = [], lEdge = []):
+        Graph_MultiPageXml.__init__(self, lNode, lEdge)
+        EdgeTransformerClassShifter.setDefaultEdgeClass([HorizontalEdge, VerticalEdge, CrossPageEdge, CrossMirrorContinuousPageVerticalEdge])
+
+    def parseXmlFile(self, sFilename, iVerbose=0):
+        """
+        Load that document as a CRF Graph.
+        Also set the self.doc variable!
+        
+        Return a CRF Graph object
+        """
+    
+        self.doc = libxml2.parseFile(sFilename)
+        self.lNode, self.lEdge = list(), list()
+        #load the block of each page, keeping the list of blocks of previous page
+        lPrevPageNode = None
+
+        for pnum, page, domNdPage in self._iter_Page_DomNode(self.doc):
+            #now that we have the page, let's create the node for each type!
+            lPageNode = list()
+            setPageNdDomId = set() #the set of DOM id
+            # because the node types are supposed to have an empty intersection
+                            
+            lPageNode = [nd for nodeType in self.getNodeTypeList() for nd in nodeType._iter_GraphNode(self.doc, domNdPage, page) ]
+            
+            #check that each node appears once
+            setPageNdDomId = set([nd.domid for nd in lPageNode])
+            assert len(setPageNdDomId) == len(lPageNode), "ERROR: some nodes fit with multiple NodeTypes"
+            
+        
+            self.lNode.extend(lPageNode)
+            
+            lPageEdge = Edge.computeEdges(lPrevPageNode, lPageNode)
+            self.lEdge.extend(lPageEdge)
+
+            lContinuousPageEdge = self.computeContinuousPageEdges(lPrevPageNode, lPageNode)
+            self.lEdge.extend(lContinuousPageEdge)
+                        
+            if iVerbose>=2: traceln("\tPage %5d    %6d nodes    %7d edges"%(pnum, len(lPageNode), len(lPageEdge)))
+            
+            lPrevPageNode = lPageNode
+        if iVerbose: traceln("\t\t (%d nodes,  %d edges)"%(len(self.lNode), len(self.lEdge)) )
+        
+        return self
+        
+
+# --------------------------------------------------------------------------------------
+class FactorialGraph_MultiContinuousPageXml(FactorialGraph_MultiPageXml, ContinousPage):
+    """
+    FactorialCRF for MultiPageXml document
+    
+    we also compute edges between continuous pages.
+    
+    FOR NOW: SIMPLE APPROXIMATE METHOD: we take N "last" block of the page and M "first" blocks of next page, and look for horizontal mirror overlap.
+    
+    """
+    def __init__(self, lNode = [], lEdge = []):
+        FactorialGraph_MultiPageXml.__init(lNode, lEdge)
+        
+        EdgeTransformerClassShifter.setDefaultEdgeClass([HorizontalEdge, VerticalEdge, CrossPageEdge, CrossMirrorContinuousPageVerticalEdge])
+
+    def parseXmlFile(self, sFilename, iVerbose=0):
+        """
+        Load that document as a CRF Graph.
+        Also set the self.doc variable!
+        
+        Return a CRF Graph object
+        """
+    
+        self.doc = libxml2.parseFile(sFilename)
+        self.lNode, self.lEdge = list(), list()
+        #load the block of each page, keeping the list of blocks of previous page
+        lPrevPageNode = None
+
+        nodeType0 = self.getNodeTypeList()[0]    #all nodes have same type
+        
+        for pnum, page, domNdPage in self._iter_Page_DomNode(self.doc):
+            #now that we have the page, let's create the node for each type!
+            lPageNode = list()
+            setPageNdDomId = set() #the set of DOM id
+            # because the node types are supposed to have an empty intersection
+                            
+            lPageNode = nodeType0._iter_GraphNode(self.doc, domNdPage, page)
+            
+            #check that each node appears once
+            setPageNdDomId = set([nd.domid for nd in lPageNode])
+            assert len(setPageNdDomId) == len(lPageNode), "ERROR: some nodes fit with multiple NodeTypes"
+            
+        
+            self.lNode.extend(lPageNode)
+            
+            lPageEdge = Edge.computeEdges(lPrevPageNode, lPageNode)
+            self.lEdge.extend(lPageEdge)
+
+            lContinuousPageEdge = self.computeContinuousPageEdges(lPrevPageNode, lPageNode)
+            self.lEdge.extend(lContinuousPageEdge)
+            
+            
+            if iVerbose>=2: traceln("\tPage %5d    %6d nodes    %7d edges"%(pnum, len(lPageNode), len(lPageEdge)))
+            
+            lPrevPageNode = lPageNode
+        if iVerbose: traceln("\t\t (%d nodes,  %d edges)"%(len(self.lNode), len(self.lEdge)) )
+        
+        return self
+        
         
