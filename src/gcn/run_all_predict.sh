c4cded93aa25d61a82b45912c0c0e47825f72459
Stephane Clinchant <sclincha@capraia.ad.europe.naverlabs.com>
2017-12-11 16:25:20 +0100

    refactoring multigraphNN

--- src/gcn/run_all_predict.sh
+++ src/gcn/run_all_predict.sh
@@ -1,17 +1,10 @@
 #!/usr/bin/env bash
 
-python DU_gcn_task.py --das_predict=True --configid=31 --das_predict_workflow=True --das_predict_earlystop=True
-python DU_gcn_task.py --das_predict=True --configid=5 --das_predict_workflow=True --das_predict_earlystop=True
-python DU_gcn_task.py --das_predict=True --configid=33 --das_predict_workflow=True --das_predict_earlystop=True
+python DU_gcn_task.py --das_predict=True --configid=31 --das_predict_workflow=True
+python DU_gcn_task.py --das_predict=True --configid=5 --das_predict_workflow=True
+python DU_gcn_task.py --das_predict=True --configid=33 --das_predict_workflow=True
 
 
-python DU_gcn_task.py --das_predict=True --configid=31 --das_predict_earlystop=True
-python DU_gcn_task.py --das_predict=True --configid=5  --das_predict_earlystop=True
-python DU_gcn_task.py --das_predict=True --configid=33 --das_predict_earlystop=True
-python DU_gcn_task.py --das_predict=True --configid=42 --das_predict_earlystop=True
-python DU_gcn_task.py --das_predict=True --configid=42 --das_predict_workflow=True --das_predict_earlystop=True
-
-
-
-python DU_gcn_task.py --das_predict=True --configid=47 --das_predict_workflow=True
-python DU_gcn_task.py --das_predict=True --configid=47
+python DU_gcn_task.py --das_predict=True --configid=31
+python DU_gcn_task.py --das_predict=True --configid=5
+python DU_gcn_task.py --das_predict=True --configid=33
