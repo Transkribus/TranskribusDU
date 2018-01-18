de2e0fba099c1a8597f07ce6e63b90a3cc1dfaaa
hdejean <hdejean@GOTLAND.ad.europe.naverlabs.com>
2018-01-18 13:43:54 +0100

    add unicode literals

--- TranskribusDU/config/ds_xml_def.py
+++ TranskribusDU/config/ds_xml_def.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-#
+
 # XML definitions for the DS software
 # 
 # Hervé Déjean, 2016
@@ -7,6 +7,9 @@
 # Copyright XRCE, 2016
 # READ project
 
+from __future__ import absolute_import
+from __future__ import  print_function
+from __future__ import unicode_literals
 
 ## pdftoxml tag ames
 sFontName = "font-name"
